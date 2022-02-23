f2fdf5631d378b5e20b0563bb58f31f0d76e51c8
koushik <58215686+koushikfs@users.noreply.github.com>
2022-02-23 23:06:54 +0530

    Update 222.py

--- 222.py
+++ 222.py
@@ -1,11 +1,18 @@
+import threading
+from threading import *
 import scapy.all as scapy
 from scapy.layers import http
-from scapy.layers.inet import IP, TCP
+from scapy.layers.inet import IP
 import optparse
-import webbrowser
+from time import sleep
+import os
 
-sndips = []
-t = 0
+
+import webbrowser
+# webbrowser.open("http://www.vulnweb.com/")
+# webbrowser.open("http://info.cern.ch/")
+# webbrowser.open("http://lushbrightfinebirds.neverssl.com/online")
+# webbrowser.open("http://www.columbia.edu/~fdc/sample.html")
 
 
 def arguments():
@@ -13,49 +20,74 @@ def arguments():
     arguments = optparse.OptionParser()
     arguments.add_option("-i", "--interface", metavar='\b', dest="interface",
                          help="specify the interface the packets are flowing through")
+    arguments.add_option("-s", "--secs", metavar='\b', dest="secs",
+                         help="no of secs to sniff", default=20)
     values, options = arguments.parse_args()
     if not values.interface:
         arguments.error("give arguments\n[+]Help: python3 packet_sniffer.py -h")
     return values
 
 
-def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=print_packet)
+def exitfunc():
+    os._exit(0)
 
 
-def add_parameters(srcip, dstip):
-    global t
-    global sndips
-    t = t+1
-    if t==1:
-        sndips.append([srcip, dstip, 0])
-    else:
-        for i in range(len(sndips)):
-            if sndips[i][0] == srcip and sndips[i][1] == dstip:
-                sndips[i][2] = sndips[i][2] + 1
-            else:
-                sndips.append([srcip, dstip, 1])
+class network_analyzer():
 
+    def __init__(self, interface):
+        self.interface=interface
+        self.sndips = []
+        self.t = 0
+        print("      src-ip                  dst-ip               visited")
+        # print("      src-ip                  dst-ip               src-port         dst-port ")
+        # print("|----------------------------------------------------------------------------|")
+        # self.sniff(self.interface)
+        # self.print_values(self.sndips)
 
-def print_packet(pkt):
-    try:
-        if pkt.haslayer(http.HTTPRequest):
-            print("   " + pkt[IP].src + "            " + pkt[IP].dst + "            " + str(
-                pkt[TCP].sport) + "             " + str(pkt[TCP].dport))
-            add_parameters(pkt[IP].src, pkt[IP].dst)
-            print("|----------------------------------------------------------------------------|")
-    except:
-        pass
+    def start_sniff(self):
+        self.sniff(self.interface)
+
+    def start_print(self):
+        self.print_values()
+
+    def sniff(self,interface):
+        scapy.sniff(iface=interface, store=False, prn=self.print_packet)
+
+    def add_parameters(self,srcip, dstip):
+        k = False
+        self.t = self.t+1
+        if self.t==1:
+            self.sndips.append([srcip, dstip, 1])
+        else:
+            for i in range(len(self.sndips)):
+                if self.sndips[i][0] == srcip and self.sndips[i][1] == dstip:
+                    self.sndips[i][2] = self.sndips[i][2] + 1
+                    k = True
+            if not k:
+                self.sndips.append([srcip, dstip, 1])
+
+    def print_packet(self,pkt):
+        try:
+            if pkt.haslayer(http.HTTPRequest):
+                # print("sniffed")
+                # print("   " + pkt[IP].src + "            " + pkt[IP].dst + "            " + str(
+                #     pkt[TCP].sport) + "             " + str(pkt[TCP].dport))
+                self.add_parameters(pkt[IP].src, pkt[IP].dst)
+                # print("|----------------------------------------------------------------------------|")
+        except:
+            pass
+
+    def print_values(self):
+        while True:
+            for i in self.sndips:
+                print("      " + str(i[0]) + "            " + str(i[1]) + "            " + str(i[2]))
+            self.sndips = []
+            print("|-------------------------------------------------------------|")
+            sleep(5)
+
 
 values = arguments()
-print("      src-ip                  dst-ip               src-port         dst-port ")
-print("|----------------------------------------------------------------------------|")
-webbrowser.open("http://www.vulnweb.com/")
-webbrowser.open("http://info.cern.ch/")
-webbrowser.open("http://lushbrightfinebirds.neverssl.com/online")
-webbrowser.open("http://www.columbia.edu/~fdc/sample.html")
-sniff(values.interface)
-print("      src-ip                  dst-ip               visited")
-for i in sndips:
-    print("|-----------------------------------------------------|")
-    print("      "+str(i[0])+"            "+str(i[1])+"            "+str(i[2]))
+ob1 = network_analyzer(values.interface)
+Timer(int(values.secs), exitfunc).start()
+threading.Thread(target=ob1.start_sniff).start()
+threading.Thread(target=ob1.start_print).start()
